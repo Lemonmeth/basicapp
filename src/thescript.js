68b878d3f9f870629e251e22eb52a484021eca3b
lemon <bnjbrg@pm.me>
2022-04-03 00:03:28 +0200

    sicko2

--- src/thescript.js
+++ src/thescript.js
@@ -3,13 +3,13 @@ let count = 0;
 
 // select value and buttons
 const value = document.querySelector ('#value');
-cont btns = document.querySelectorAll('#.btn');
+const btns = document.querySelectorAll('#.btn');
 
 
 
 btns.forEach(function (btn) {
     btn.addeeventlistener("click", function (e) {
-        cont styles = e.currentTarget.classlist;
+        const styles = e.currentTarget.classlist;
         if (styles.contains("decrease")) {
             count--;
         }
