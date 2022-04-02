272edfbb7b0d9b53472fef38600dc24aa1e20072
lemon <bnjbrg@pm.me>
2022-04-03 00:52:25 +0200

    kjdfg

--- src/thescript.js
+++ src/thescript.js
@@ -8,8 +8,8 @@ const btns = document.querySelectorAll('#.btn');
 
 
 btns.forEach(function (btn) {
-    btn.addeeventlistener("click", function (e) {
-        const styles = e.currentTarget.classlist;
+    btn.addEventListener("click", function (e) {
+        const styles = e.currentTarget.classList;
         if (styles.contains("decrease")) {
             count--;
         }
